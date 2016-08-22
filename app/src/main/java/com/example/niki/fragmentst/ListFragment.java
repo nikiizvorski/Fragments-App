77a367bdf933948c760e3e457b7e3227c0f8a8d5
NIKI <nikiizvorski@gmail.com>
2016-08-22 09:54:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/niki/fragmentst/ListFragment.java
@@ -0,0 +1,35 @@
+package com.example.niki.fragmentst;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by NIKI on 8/22/2016.
+ */
+public class ListFragment extends Fragment {
+
+    public interface onRecipeSelectedInterface {
+        void onListRecipeSelected(int index);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        onRecipeSelectedInterface listener = (onRecipeSelectedInterface) getActivity();
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRecycleView);
+        ListAdapter listAdapter = new ListAdapter(listener);
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager  layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+}
