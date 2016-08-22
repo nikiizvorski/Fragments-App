8c879458e31cdfdbb8319e7122ee938186b6e855
NIKI <nikiizvorski@gmail.com>
2016-08-22 10:52:16 +0100

    Added ViewPager

---
+++ app/src/main/java/com/example/niki/fragmentst/ViewPagerFragment.java
@@ -0,0 +1,20 @@
+package com.example.niki.fragmentst;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by NIKI on 8/22/2016.
+ */
+public class ViewPagerFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+    }
+}
